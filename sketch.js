46b51f73564256589511369523a0ddc505859cd2
elputnam <elputnam@inaction.ie>
2021-10-02 19:36:38 +0100

    added second line

--- sketch.js
+++ sketch.js
@@ -1,11 +1,13 @@
 let capture;
 let base;
+let base1;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   colorMode(HSB, 360, 100, 100, 100);
-  // frameRate(5);
+  frameRate(10);
   base = height;
+  base1 = 0;
   //web cam capture
   capture = createCapture(VIDEO);
   capture.size(640, 480);
@@ -13,17 +15,24 @@ function setup() {
 }
 
 function draw() {
-  background(150, random(100), random(100), 5);
+  background(360, random(100), random(100), 5);
   imageMode(CENTER);
   filter(INVERT);
-  tint(360, random(100), random(100));
+  tint(175, random(100), random(100));
   image(capture, random(width), random(height), random(width), random(height/2));
+  // image(capture, width/2, height/2, random(width), random(height/2));
   // filter(OPAQUE);
   stroke(random(255));
   strokeWeight(10);
   line(0, base, width, base);
-  base -= 1;
-  if (base == 0){
+  line(0, base1, width, base1);
+  let num = 10
+  base -= random(-num, num);
+  base1 += random(-num, num);
+  if (base <= 0){
     base = height;
+  }
+  if (base1 >= height){
+    base1 = 0;
   }
 }
